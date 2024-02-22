f21a53b49d7d4b2ef249fc4d2299177406106fd9
shilpamk <shilpa.vairagi@gmail.com>
2024-02-22 09:36:33 +0000

    Deposite function added.

--- bank_accounts.py
+++ bank_accounts.py
@@ -5,6 +5,13 @@ class BankAccount:
         self.name = acctName
         print(f"\nAccount '{self.name}' created\nBalance = ${self.balance:.2f}\n" )
 
-    def get_balnce(self):
+    def get_balance(self):
         print(f"Account '{self.name}' balance = ${self.balance:.2f}\n")
+
+    def deposit(self, amount):
+        print(f"Deposit amount = ${amount}")
+        self.balance = self.balance + amount
+        print(f"Deposit complete")
+        self.get_balance()
+        
 
--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from bank_accounts import *
 p1 = BankAccount(1000, "Shilpa")
 p2 = BankAccount(2000, "Manju")
 
-p1.get_balnce()
+p1.get_balance()
+p1.deposit(500)
+
+p2.get_balance()
+p2.deposit(200)
